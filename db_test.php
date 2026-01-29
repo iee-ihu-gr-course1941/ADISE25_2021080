c8aa5a9d8fabc33b99ddf3ddde57ebd1b2d8a6ee
dimitramakri <dimitramakri03@gmail.com>
2026-01-30 00:21:34 +0200

    Update db_test.php

--- db_test.php
+++ db_test.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $socket = "/home/staff/iee2021080/mysql/run/mysql.sock";
 $conn = new mysqli(null, "iee2021080", "Ο_ΚΩΔΙΚΟΣ_ΣΟΥ", "ADISEDB", null, $socket);
 
