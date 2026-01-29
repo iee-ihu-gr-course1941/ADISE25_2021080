598bd106c5ddea25cb14ed200d270b133e217ca9
dimitramakri <dimitramakri03@gmail.com>
2026-01-30 00:29:26 +0200

    Update db_test.php

--- db_test.php
+++ db_test.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $socket = "/home/staff/iee2021080/mysql/run/mysql.sock";
-$conn = new mysqli(null, "iee2021080", "Ο_ΚΩΔΙΚΟΣ_ΣΟΥ", "ADISEDB", null, $socket);
+$conn = new mysqli(null, "iee2021080", "1718Dim3##", "ADISEDB", null, $socket);
 
 if ($conn->connect_error) {
     die("Σφάλμα σύνδεσης: " . $conn->connect_error);
