30e060310dd27291b0419db835d40afcd2daeb75
dimitramakri <dimitramakri03@gmail.com>
2026-01-30 00:40:36 +0200

    Update database.php

--- Api/config/database.php
+++ Api/config/database.php
@@ -1,32 +1,33 @@
 <?php
 class Database {
-  $socket = "/home/student/iee/2021/iee2021080/mysql/run/mysql.sock";
-$conn = new mysqli(null, "iee2021080", "1718Dim3##", "ADISEDB", null, $socket);
-
+    private $username = "iee2021080";
+    private $password = "1718Dim3##";
+    private $db_name  = "ADISEDB";
+    private $socket   = "/home/student/iee/2021/iee2021080/mysql/run/mysql.sock";
     public $conn;
-   
+
     public function __construct() {
         $this->connect();
     }
-    
+
     public function connect() {
-        try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-                ]
-            );
-            return $this->conn;
-        } catch(PDOException $e) {
-            throw new Exception("Database connection failed: " . $e->getMessage());
+        $this->conn = new mysqli(
+            null,               // host null για socket
+            $this->username,
+            $this->password,
+            $this->db_name,
+            null,               // port null
+            $this->socket
+        );
+
+        if ($this->conn->connect_error) {
+            die("Σφάλμα σύνδεσης: " . $this->conn->connect_error);
         }
+
+        // Ορίστε UTF-8
+        $this->conn->set_charset("utf8");
     }
-    
+
     public function getConnection() {
         return $this->conn;
     }
